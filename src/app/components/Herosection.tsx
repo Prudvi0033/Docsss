412f43804b25faccc05b3429254f5c3e377476bb
Prudvi <prudvirajvarma2004@gmail.com>
2025-05-04 18:16:37 +0530

    Added new changes

--- src/app/components/Herosection.tsx
+++ src/app/components/Herosection.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Raleway } from 'next/font/google'
 import { Oswald } from 'next/font/google'
 import PencilAnimation from './PencilAnimation'
+import { CgLoadbarDoc } from "react-icons/cg";
+
 
 const os = Oswald({ subsets: ['latin'] })
 const rale = Raleway({ subsets: ['latin'] })
@@ -9,23 +11,21 @@ const rale = Raleway({ subsets: ['latin'] })
 const Herosection = () => {
     return (
         <div>
-            <div className={`flex flex-col leading-none mt-10 ml-12 items-start ${rale.className}`}>
+            <div className={`flex flex-col leading-none mt-4 ml-14 items-start ${rale.className}`}>
 
                 {/* Heading section */}
-                <div className={`flex font-extrabold gap-x-16 text-[20rem] ${rale.className}`}>
-                    <div className={`flex font-extrabold gap-x-16 text-[20rem] ${rale.className}`}>
-                        <h3 className="relative text-transparent bg-clip-text" style={{
-                            WebkitTextStroke: "1px black"
-                        }}>
+                <div className={`flex font-extrabold gap-x-16 ${rale.className}`}>
+                    <div className={`flex items-start leading-none flex-col font-extrabold gap-x-16 text-[20rem] ${rale.className}`}>
+                        <h3 className="relative text-9xl bg-gradient-to-br from-yellow-400 p-2 rounded-3xl">
                             The
                         </h3>
-                        <h1>
+                        <h3 className='text-[16rem]'>
                             Docs
-                        </h1>
+                        </h3>
                     </div>
                 </div>
 
-                <div className="flex justify-between items-center mt-12 px-8 w-full">
+                <div className="flex justify-between items-center px-8 w-full">
                     <p className="text-base leading-6 text-black max-w-2xl">
                         <span className='font-semibold'>Collaborate</span> seamlessly in <span className='text-red-900 font-semibold'>real time</span> with Docs’ rich text editor. Powered by AI, it helps you write faster, keep content clear, and <span className="italic">summarize your work</span> to save time and boost productivity.
                     </p>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,11 +9,11 @@ const Navbar = () => {
     return (
         <div className=' sticky top-0 z-50'>
             <div className='flex items-center justify-center p-6'>
-                <div className='bg-white/10 backdrop-blur-lg rounded-full shadow-xl px-12 border border-white/20'>
+                <div className='bg-white/10 backdrop-blur-lg rounded-full shadow-xl px-8 border border-white/20'>
                     <div className='flex p-2 gap-6 text-neutral-800/90'>
-                    <GoHomeFill size={44} className='cursor-pointer hover:bg-white/60 rounded-full p-2 transition-all' />
-                    <MdBookmarks size={44} className='cursor-pointer hover:bg-white/60 rounded-full p-2 transition-all' />
-                    <RiStickyNoteAddLine size={44} className='cursor-pointer hover:bg-white/60 rounded-full p-2 transition-all' />
+                    <GoHomeFill size={34} className='cursor-pointer hover:bg-white/60 rounded-full p-2 mt-1 transition-all' />
+                    <MdBookmarks size={34} className='cursor-pointer hover:bg-white/60 rounded-full p-2 mt-1  transition-all' />
+                    <RiStickyNoteAddLine size={34} className='cursor-pointer hover:bg-white/60 rounded-full p-2 mt-1  transition-all' />
 
                     <div className='h-10 w-px bg-neutral-300 mx-1'></div></div>
                 </div>
