296682872921e39f095c68d1285a77a39d71863a
Prudvi <prudvirajvarma2004@gmail.com>
2025-05-04 22:45:05 +0530

    Added bottom img

--- src/app/components/Herosection.tsx
+++ src/app/components/Herosection.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Raleway } from 'next/font/google'
 import { Oswald } from 'next/font/google'
 import PencilAnimation from './PencilAnimation'
-import { CgLoadbarDoc } from "react-icons/cg";
-
+import Image from 'next/image'
 
 const os = Oswald({ subsets: ['latin'] })
 const rale = Raleway({ subsets: ['latin'] })
@@ -11,26 +10,33 @@ const rale = Raleway({ subsets: ['latin'] })
 const Herosection = () => {
     return (
         <div>
-            <div className={`flex flex-col leading-none mt-4 ml-14 items-start ${rale.className}`}>
+            <div className={`flex flex-col mt-4 ml-14 ${rale.className}`}>
 
-                {/* Heading section */}
-                <div className={`flex font-extrabold gap-x-16 ${rale.className}`}>
-                    <div className={`flex items-start leading-none flex-col font-extrabold gap-x-16 text-[20rem] ${rale.className}`}>
-                        <h3 className="relative text-9xl bg-gradient-to-br from-yellow-400 p-2 rounded-3xl">
-                            The
-                        </h3>
-                        <h3 className='text-[16rem]'>
-                            Docs
-                        </h3>
+                <div className="flex flex-col leading-38 items-start gap-8 text-[10rem] font-extrabold">
+                    <div className='flex items-center gap-6'>
+                        <span>The</span>
+
+                        <div className="bg-black rounded-[10rem] overflow-hidden w-[240px] h-[120px] flex items-center justify-center">
+                            <Image
+                                src="/hand.jpeg"
+                                width={240}
+                                height={120}
+                                alt="img"
+                                className="object-cover"
+                            />
+                        </div>
                     </div>
+
+                    <span className="italic text-[14rem]">Docs</span>
                 </div>
 
-                <div className="flex justify-between items-center px-8 w-full">
-                    <p className="text-base leading-6 text-black max-w-2xl">
-                        <span className='font-semibold'>Collaborate</span> seamlessly in <span className='text-red-900 font-semibold'>real time</span> with Docs’ rich text editor. Powered by AI, it helps you write faster, keep content clear, and <span className="italic">summarize your work</span> to save time and boost productivity.
-                    </p>
-                    <div className='mr-16'>
-                        <PencilAnimation />
+                {/* Description + Pencil */}
+                <div className="flex justify-between items-center mt-12 px-8 w-full">
+                    <div className="mr-16">
+                        <div className="w-[85rem] h-42 rounded-3xl bg-black overflow-hidden flex items-center justify-center">
+                             <Image src="/keyboard.jpeg" className='object-cover bg-black' width={1550} height={32} alt='img' />   
+                        </div>
+                        
                     </div>
                 </div>
             </div>
