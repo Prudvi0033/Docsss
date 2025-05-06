5ade29df5add481e57ce7060e58865d71625b6bf
Prudvi <prudvirajvarma2004@gmail.com>
2025-05-06 15:13:23 +0530

    Adde new center

--- src/app/components/Herosection.tsx
+++ src/app/components/Herosection.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Raleway } from 'next/font/google'
-import { Oswald } from 'next/font/google'
+import { Poiret_One } from 'next/font/google'
 import PencilAnimation from './PencilAnimation'
 import Image from 'next/image'
 
-const os = Oswald({ subsets: ['latin'] })
+const os = Poiret_One({ subsets: ['latin'], weight: "400" })
 const rale = Raleway({ subsets: ['latin'] })
 
 const Herosection = () => {
     return (
         <div>
-            <div className={`flex flex-col mt-4 ml-14 ${rale.className}`}>
+            <div className={`flex flex-col mt-8 ${rale.className}`}>
 
-                <div className="flex flex-col leading-38 items-start gap-8 text-[10rem] font-extrabold">
+                <div className="flex flex-col items-center leading-24 gap-8 text-[8rem] font-light">
                     <div className='flex items-center gap-6'>
                         <span>The</span>
 
@@ -27,18 +27,10 @@ const Herosection = () => {
                         </div>
                     </div>
 
-                    <span className="italic text-[14rem]">Docs</span>
+                    <div className="flex text-[12rem]">D <div><PencilAnimation /></div> cs</div>
                 </div>
 
-                {/* Description + Pencil */}
-                <div className="flex justify-between items-center mt-12 px-8 w-full">
-                    <div className="mr-16">
-                        <div className="w-[85rem] h-42 rounded-3xl bg-black overflow-hidden flex items-center justify-center">
-                             <Image src="/keyboard.jpeg" className='object-cover bg-black' width={1550} height={32} alt='img' />   
-                        </div>
-                        
-                    </div>
-                </div>
+
             </div>
         </div>
     )
